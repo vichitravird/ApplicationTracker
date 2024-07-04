be0b7dcf412072998ae6a44b7d5b298b611db1f9
vichitravird <93407819+vichitravird@users.noreply.github.com>
2024-07-04 16:40:06 -0500

    Initial commit with HTML and JavaScript for Application Tracker

---
+++ script.js
@@ -0,0 +1,11 @@
+function appTracker() {
+    window.open('https://forms.gle/ttuHEVi725RMhQf17', '_blank'); // Opens the link in a new tab
+}
+
+function intTracker() {
+    window.open('https://forms.gle/AhMQkJ1uVHbgBNVn8', '_blank'); // Opens the link in a new tab
+}
+
+function analysisTracker() {
+    window.open('https://docs.google.com/spreadsheets/d/1TY0G_WjYtZ6YSbRNkN-LVqnd8U-HffYGzCgH54CZCAo/edit?usp=sharing', '_blank'); // Opens the link in a new tab
+}
