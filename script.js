e9ddfc5c89aa8ceb37c26323c03b9fce8ab710ae
vichitravird <93407819+vichitravird@users.noreply.github.com>
2024-07-04 21:20:37 -0500

    Change 1

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function intTracker() {
 function analysisTracker() {
     window.open('https://docs.google.com/spreadsheets/d/1TY0G_WjYtZ6YSbRNkN-LVqnd8U-HffYGzCgH54CZCAo/edit?usp=sharing', '_blank'); // Opens the link in a new tab
 }
+
+function updateDateTime() {
+    const now = new Date();
+    const formattedDateTime = now.toLocaleString();
+    document.getElementById('datetime').textContent = formattedDateTime;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime(); // Initial call to display the time immediately
